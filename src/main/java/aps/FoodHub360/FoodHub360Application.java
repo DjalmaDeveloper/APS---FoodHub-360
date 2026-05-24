3d592f06c885239fecc93371f0abe844a107394b
Djalma <djalma.ant@gmail.com>
2026-05-24 14:52:31 -0300

    First Commit

---
+++ src/main/java/aps/FoodHub360/FoodHub360Application.java
@@ -0,0 +1,13 @@
+package aps.FoodHub360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodHub360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodHub360Application.class, args);
+	}
+
+}
---
+++ src/test/java/aps/FoodHub360/FoodHub360ApplicationTests.java
@@ -0,0 +1,13 @@
+package aps.FoodHub360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodHub360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
